7f816f696a36f3a26fba930ed3b6818500876e09
roeljohn <bobisroeljohn@gmail.com>
2024-01-12 10:13:21 +0800

    create PostPreview component

---
+++ src/app/components/postPreview.js
@@ -0,0 +1,17 @@
+
+export default function PostPreview() {
+    return(
+        <div className="post-preview">
+                <a href="post.html">
+                    <h2 className="post-title">Man must explore, and this is exploration at its greatest</h2>
+                    <h3 className="post-subtitle">Problems look mighty small from 150 miles up</h3>
+                </a>
+                <p className="post-meta">
+                    Posted by
+                    <a href="#!">Start Bootstrap</a>
+                    on September 24, 2023
+                </p>
+            <hr className="my-4" />
+        </div>
+    )
+}
